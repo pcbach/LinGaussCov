308ced4e06c77b9d5c72d5fb22b3b05acb6883b8
chi bach pham <p.chi.bach@gmail.com>
2021-05-01 23:18:35 +1000

    1/05/21 Total Revamp

--- ps.py
+++ ps.py
@@ -8,58 +8,61 @@ import cvxpy as cp
 import scipy as sp
 import time
 import os 
-
+#input double/numpy array
 def f(x):
     return 1/x + np.log(x)
+#output double/numpy array
 
-def linearLagrangeInterpolate(xa,xb):
+#input 2 double
+def linearLagrangeInterpolateF(xa,xb):
     M = np.array([[xa, 1],
                   [xb, 1]])
     B = np.array([f(xa),f(xb)])
     A = np.linalg.solve(M, B)
     return A
+#output numpy array (1,2)
 
-def cubicLagrangeInterpolate(xa,xb,xc,xd):
-    M = np.array([[xa**3, xa**2, xa, 1],
-                  [xb**3, xb**2, xb, 1],
-                  [xc**3, xc**2, xc, 1],
-                  [xd**3, xd**2, xd, 1]])
-    B = np.array([f(xa),f(xb),f(xc),f(xd)])
+#input 2 double
+def linearLagrangeInterpolateLog(xa,xb):
+    M = np.array([[xa, 1],
+                  [xb, 1]])
+    B = np.array([np.log(xa),np.log(xb)])
     A = np.linalg.solve(M, B)
     return A
+#output numpy array (1,2)
 
+#input number
 def linearEstimatorError(begin, end):
+    #swap end
     if begin > end:
         begin, end = end, begin
-    a = (f(end) - f(begin))/(end-begin)
-    b = f(end) - a*end
-    c = 2.0/(1.0 + np.sqrt(1.0 - 4.0 * a)) 
-    return abs(np.log(c) + 1/c - ( a*c +b))
 
-def cubicEstimatorError(begin,end):
-    if begin < end:
-        xa = begin
-        xd = end
-    else:
-        xa = end
-        xd = begin
-    xb = xa + 1/4*(xd-xa)
-    xc = xa + 3/4*(xd-xa)
-    A = cubicLagrangeInterpolate(xa,xb,xc,xd)
-    X = np.roots([-3*A[0],-2*A[1],-A[2],1,-1])
-    X = X[X>0]
-    error = abs(f(X) - (A[0]*X**3 + A[1]*X**2 + A[2]*X + A[3])) 
-    return max(error)
+    [a,b] = linearLagrangeInterpolateF(begin,end)
+    #y = ax + b 
+    c = (1.0 - np.sqrt(1.0 - 4.0 * a))/(2*a)
+    return abs(np.log(c) + 1/c - ( a*c +b)),c
 
-def search(begin, end, precision, estimationType = 'Linear'):
+def altLinEstimatorError(begin, end):
+    if begin > end:
+        begin, end = end, begin
+    a = (np.log(end) - np.log(begin))/(end-begin)
+    b = -a*begin + np.log(begin)
+    c = 1/a
+    return abs(np.log(c) - (a*c + b)),c
+
+def search(begin, end, precision, estimationType = 'Linear', deltaType = 'max'):
     pivot = begin
     while abs(begin - end) > 1e-9:
         mid = (begin + end)/2
 
-        if estimationType == 'Cubic':
-            error = cubicEstimatorError(pivot,mid)
+        if estimationType == 'AltLin':
+            error,xerr = altLinEstimatorError(pivot,mid)
+            if deltaType == 'snr':
+                error = error/f(xerr)
         else:
-            error = linearEstimatorError(pivot,mid)
+            error,xerr = linearEstimatorError(pivot,mid)
+            if deltaType == 'snr':
+                error = error/f(xerr)
 
         if error <= precision:
             begin = mid
@@ -67,24 +70,24 @@ def search(begin, end, precision, estimationType = 'Linear'):
             end = mid
     return (begin + end)/2
 
-def approximate(precision,eps,estimationType = 'Linear'):
-    file = 'Type' + estimationType + '-Precision' + str(precision) + '-Epsilon' + str(eps) + '.npy'
-    if os.path.isfile(os.path.join('preset', file)):
-        lines  = np.load(os.path.join('preset', file))
-    else:
-
-        lines = []
-        curr = 2
-        while curr - eps > 1e-9:
-            next = search(curr,eps,precision,estimationType)
-            if estimationType == 'Cubic':
-                c = [curr, next+3/4*(curr-next), next+1/4*(curr-next), next]
-            else:
-                c = linearLagrangeInterpolate(next,curr)
-            lines.append(c)
-            curr = next
-        np.save(os.path.join('preset', file), lines)
-    return np.array(lines)
+def approximate(precision,begin,end,estimationType,deltaType = 'max'):
+    #print(estimationType)
+    waypoint = []
+    lines = []
+    curr = end
+    waypoint.append(curr)
+    while curr > begin + 1e-9:
+        #print(curr)
+        if estimationType == 'AltLin':
+            next = search(curr,begin,precision,'AltLin',deltaType)
+            c = linearLagrangeInterpolateLog(next,curr) 
+        elif estimationType == 'Linear':
+            next = search(curr,begin,precision,'Linear',deltaType)
+            c = linearLagrangeInterpolateF(next,curr)   
+        lines.append(c)
+        waypoint.append(next)
+        curr = next
+    return np.array(lines),np.array(waypoint)
 
 def Sn(data):
     d = data[0].shape[0]
@@ -99,75 +102,76 @@ def Sn(data):
 def is_pos_def(x):
     return np.all(np.linalg.eigvals(x) > 0)
 
+
 class LGC:
-    def __init__(self,X,dimension,precision = 1e-4,epsilon = 5e-3,estimationType = 'linear'):
-        self.h = precision
+    def __init__(self,cov,dimension, data = [],delta = 1e-3,epsilon = 5e-3,mu = 1e-2, errorType = 'max',debug = False):
+        self.dlt = delta
         self.eps = epsilon
         self.dim = dimension
+        self.errorType = errorType
+        self.mu = mu
         self.constraints = []
+        self.data = data
         self.I = np.eye(self.dim)
-        self.estimationType = estimationType
         self.size = 0
-        self.X = X
+        self.cov = cov
+        self.X = cp.Variable((self.dim,self.dim), PSD = True)
         self.Y = cp.Variable((self.dim,self.dim))
         self.t = cp.Variable()
-        self.alpha1 = []
-        self.alpha2 = []
-        self.alpha3 = []
-        self.alpha4 = []
+        self.lamd_1 = cp.Variable((self.dim,self.dim))
+        self.lamd_2 = cp.Variable((self.dim,self.dim))
+        self.X_2 = cp.Variable((self.dim,self.dim))
+        self.Y_2 = cp.Variable((self.dim,self.dim))
+        self.X_1 = cp.Variable((self.dim,self.dim))
+        self.Y_1 = cp.Variable((self.dim,self.dim))
         self.X_ = []
-        self.Y_ = []
-        self.Z_ = []
+        self.lamd = []
+        self.tau = []
+        self.taup = []
+        self.debug = debug
 
     def setup_constraint(self):
-        lines = approximate(self.h,self.eps,self.estimationType)
-        self.size = len(lines)
-        if self.estimationType == 'Cubic':
-            for i in range(self.size):
-                self.alpha1.append(cp.Variable((self.dim,self.dim)))
-                self.alpha2.append(cp.Variable((self.dim,self.dim)))
-                self.alpha3.append(cp.Variable((self.dim,self.dim)))
-                self.alpha4.append(cp.Variable((self.dim,self.dim)))
-                self.X_.append(cp.Variable((self.dim,self.dim), PSD = True))
-                self.Y_.append(cp.Variable((self.dim,self.dim)))
-                self.Z_.append(cp.Variable((self.dim,self.dim)))
-            for i in range(self.size):
-                Xi = np.flip(lines[i]).tolist()
-                Yi = np.flip(f(lines[i])).flatten().tolist()
-                self.constraints += [cp.bmat ([ 
-                                                [3.0 * self.alpha2[i],   0.0 * self.I        ],
-                                                [0.0 * self.I        ,  12.0 * self.alpha4[i]]
-                                        ]) + 
-                            cp.bmat     ([      [1.0 * self.alpha3[i],   2.0 * self.alpha2[i]],
-                                                [2.0 * self.alpha3[i],   4.0 * self.alpha3[i]]
-                                    ]) >> 0 * np.eye(2 * self.dim)]
+        self.sigma = Sn(self.data)
+        self.sqrt_sigma = sp.linalg.sqrtm(self.sigma)
 
-                self.constraints += [cp.bmat ([ 
-                                                [3.0 * self.alpha3[i],   0.0 * self.I        ],
-                                                [0.0 * self.I        ,  12.0 * self.alpha1[i]]
-                                        ]) + \
-                            cp.bmat     ([      [1.0 * self.alpha2[i],   2.0 * self.alpha2[i]],
-                                                [2.0 * self.alpha2[i],   4.0 * self.alpha3[i]]
-                                    ]) >> 0 * np.eye(2 * self.dim)]
+        self.linesFrL,self.waypointFrL = approximate(self.dlt,self.eps,self.mu,'AltLin',self.errorType)
+        self.linesLin,self.waypointLin = approximate(self.dlt,self.mu,2,'Linear',self.errorType)
+        if (self.debug):
+            print('{} alternate segments'.format(len(self.linesFrL)))
+            print('{} linear segments'.format(len(self.linesLin)))
+        #setup array variables
+        for j in range(len(self.linesFrL)):
+            self.lamd.append(cp.Variable((self.dim,self.dim), PSD = True))
+            self.X_.append(cp.Variable((self.dim,self.dim), PSD = True))
+            self.taup.append(cp.Variable((self.dim,self.dim), PSD = True))
+            self.tau.append(cp.Variable((self.dim,self.dim), PSD = True))
 
-                self.constraints += [self.alpha1[i] + self.alpha2[i] + self.alpha3[i] + self.alpha4[i] == self.Z_[i]]
-                self.constraints += [self.alpha1[i] * Xi[0]  + self.alpha2[i] * Xi[1]  + self.alpha3[i] * Xi[2]  + self.alpha4[i] * Xi[3]  == self.X_[i]]
-                self.constraints += [self.alpha1[i] * Yi[0]  + self.alpha2[i] * Yi[1]  + self.alpha3[i] * Yi[2]  + self.alpha4[i] * Yi[3]  << self.Y_[i]]
-                self.constraints += [self.Z_[i] >> 0 * self.I]
-            self.constraints += [self.X << 2*self.I]
-            self.constraints += [self.X >> self.eps*self.I]
-            self.constraints += [cp.sum(self.X_) == self.X]
-            self.constraints += [cp.sum(self.Y_) == self.Y]
-            self.constraints += [cp.sum(self.Z_) == self.I]
-        else:
-            a = lines[:,0]
-            c = lines[:,1]
-            self.constraints += [a[i] * self.X - self.Y << -c[i] * self.I for i in range(self.size)]  
+        #AltLin portion
+        for j in range(len(self.linesFrL)):
+            a = self.linesFrL[j][0]
+            b = self.linesFrL[j][1]
+            self.constraints += [cp.bmat ([   [self.X_[j],self.lamd[j]],[self.lamd[j],self.taup[j]]]) >> 0 * np.eye(2 * self.dim)]
+            self.constraints += [self.taup[j] + a*self.X_[j] + b*self.lamd[j] << self.tau[j]]
+
+        self.constraints += [cp.sum(self.X_) == self.X_1]
+        self.constraints += [cp.sum(self.tau) == self.Y_1]
+        self.constraints += [cp.sum(self.lamd) == self.lamd_1]
+
+        #Linear Portion
+        a = self.linesLin[:,0]
+        c = self.linesLin[:,1]
+        self.constraints += [a[i] * self.X_2 - self.Y_2 << -c[i] * self.lamd_2 for i in range(len(self.linesLin))]
+
+        self.constraints += [self.lamd_1 + self.lamd_2 == self.I]
+        self.constraints += [self.X_1 + self.X_2 == self.X]
+        self.constraints += [self.Y_1 + self.Y_2 == self.Y]
+
+
         self.constraints += [self.X << 2*self.I]
         self.constraints += [self.X >> self.eps*self.I]
         self.constraints += [cp.trace(self.Y) <= self.t]
 
-        
+        self.constraints += [self.sqrt_sigma @ self.X @ self.sqrt_sigma == self.cov]
 
     def add_constraint(self, constraints):
         self.constraints += constraints
@@ -176,4 +180,5 @@ class LGC:
         self.setup_constraint()
         prob = cp.Problem(cp.Minimize(self.t),self.constraints)
         prob.solve(solver = solver, verbose = verbose)
-        return self.t.value, self.X.value
+        self.time = prob.solver_stats.solve_time
+        return self.t.value, self.cov.value, self.time
