c70623776f3b16ffe6657c9f0cae422975b20816
chi bach pham <p.chi.bach@gmail.com>
2021-05-10 22:10:10 +1000

    Ducumentation added

--- ps.py
+++ ps.py
@@ -1,184 +1,273 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import animation
-# %pip install cvxpy
-# %pip install scipy
-import cvxpy as cp
-import scipy as sp
-import time
-import os 
-#input double/numpy array
-def f(x):
-    return 1/x + np.log(x)
-#output double/numpy array
+import cvxpy
+import numpy
+import scipy
+import mosek
 
-#input 2 double
-def linearLagrangeInterpolateF(xa,xb):
-    M = np.array([[xa, 1],
-                  [xb, 1]])
-    B = np.array([f(xa),f(xb)])
-    A = np.linalg.solve(M, B)
-    return A
-#output numpy array (1,2)
 
-#input 2 double
-def linearLagrangeInterpolateLog(xa,xb):
-    M = np.array([[xa, 1],
-                  [xb, 1]])
-    B = np.array([np.log(xa),np.log(xb)])
-    A = np.linalg.solve(M, B)
-    return A
-#output numpy array (1,2)
+def f(x: float) -> float:
+    """
+    return value of function 1/x + log(x)
+    :param x: x
+    :return: f(x)
+    """
+    return 1 / x + numpy.log(x)
 
-#input number
-def linearEstimatorError(begin, end):
-    #swap end
+
+def lineParameterF(xa: float, xb: float) -> numpy.ndarray:
+    """
+    return value of the line y = ax + b 
+    that run through the points (xa,f(xa)), (xb,f(xb))
+    :param xa: begin
+    :param xb: end
+    :return: array [a, b] for line y = ax + b
+    """
+    m: numpy.ndarray = numpy.array([[xa, 1],
+                                    [xb, 1]])
+    b: numpy.ndarray = numpy.array([f(xa), f(xb)])
+    a: numpy.ndarray = numpy.linalg.solve(m, b)
+    return a
+
+
+def lineParameterLog(xa: float, xb: float) -> numpy.ndarray:
+    """
+    return value of the line y = ax + b
+    that run through the points (xa,ln(xa)), (xb,ln(xb))
+    :param xa: begin
+    :param xb: end
+    :return: array [a, b] for line y = ax + b
+    """
+    m: numpy.ndarray = numpy.array([[xa, 1],
+                                    [xb, 1]])
+    b: numpy.ndarray = numpy.array([numpy.log(xa), numpy.log(xb)])
+    a: numpy.ndarray = numpy.linalg.solve(m, b)
+    return a
+
+
+def linearEstimatorError(begin: float, end: float) -> (float, float):
+    """
+    return the maximum absolute different
+    between the line that run through the
+    points (begin, f(begin)) and (end, f(end))
+    and function f in [begin, end]
+    :param begin: begin
+    :param end: end
+    :return: error value and its position
+    """
+    # swap end
     if begin > end:
         begin, end = end, begin
 
-    [a,b] = linearLagrangeInterpolateF(begin,end)
-    #y = ax + b 
-    c = (1.0 - np.sqrt(1.0 - 4.0 * a))/(2*a)
-    return abs(np.log(c) + 1/c - ( a*c +b)),c
+    # get line parameter
+    [a, b] = lineParameterF(begin, end)
+    # y = ax + b
+    c = (1.0 - numpy.sqrt(1.0 - 4.0 * a)) / (2 * a)
+    return abs(numpy.log(c) + 1 / c - (a * c + b)), c
 
-def altLinEstimatorError(begin, end):
+
+def altLinEstimatorError(begin: float, end: float) -> (float, float):
+    """
+    return the maximum absolute different
+    between the line that run through the
+    points (begin, ln(begin)) and (end, ln(end))
+    and ln in [begin, end]
+    :param begin: begin
+    :param end: end
+    :return: error value and its position
+    """
+    # swap end
     if begin > end:
         begin, end = end, begin
-    a = (np.log(end) - np.log(begin))/(end-begin)
-    b = -a*begin + np.log(begin)
-    c = 1/a
-    return abs(np.log(c) - (a*c + b)),c
+    # get line parameter
+    a = (numpy.log(end) - numpy.log(begin)) / (end - begin)
+    b = -a * begin + numpy.log(begin)
+    # y = ax + b
+    c = 1 / a
+    return abs(numpy.log(c) - (a * c + b)), c
 
-def search(begin, end, precision, estimationType = 'Linear', deltaType = 'max'):
+
+def search(begin: float, end: float, delta: float, estimation_type: str = 'Linear', delta_type: str = 'max') -> float:
+    """
+    search for point x' that satisfy:
+    - x' is in [begin, end]
+    - error of line through (x', f(x')) and (begin, f(begin)) <= delta
+    - x' is min
+    :param begin: begin
+    :param end: end
+    :param delta: delta
+    :param estimation_type: either 'Linear' or 'AltLin'
+    :param delta_type: either 'max' or 'snr'
+    :return:
+    """
     pivot = begin
     while abs(begin - end) > 1e-9:
-        mid = (begin + end)/2
+        mid = (begin + end) / 2
 
-        if estimationType == 'AltLin':
-            error,xerr = altLinEstimatorError(pivot,mid)
-            if deltaType == 'snr':
-                error = error/f(xerr)
+        if estimation_type == 'AltLin':
+            [error, x_err] = altLinEstimatorError(pivot, mid)
+            if delta_type == 'snr':
+                error = error / f(x_err)
         else:
-            error,xerr = linearEstimatorError(pivot,mid)
-            if deltaType == 'snr':
-                error = error/f(xerr)
+            error, x_err = linearEstimatorError(pivot, mid)
+            if delta_type == 'snr':
+                error = error / f(x_err)
 
-        if error <= precision:
+        if error <= delta:
             begin = mid
         else:
             end = mid
-    return (begin + end)/2
+    return (begin + end) / 2
 
-def approximate(precision,begin,end,estimationType,deltaType = 'max'):
-    #print(estimationType)
+
+def approximate(delta: float, begin: float, end: float,
+                estimation_type: str, delta_type: str = 'max') -> (numpy.ndarray, numpy.ndarray):
+    """
+    return the function f(x) representation
+    :param delta: delta
+    :param begin: begin
+    :param end: end
+    :param estimation_type: either 'Linear' or 'AltLin'
+    :param delta_type: either 'max' or 'snr'
+    :return: first array contain the line parameter, the second array contain the endpoints of segment
+    """
+    # print(estimationType)
     waypoint = []
     lines = []
     curr = end
     waypoint.append(curr)
     while curr > begin + 1e-9:
-        #print(curr)
-        if estimationType == 'AltLin':
-            next = search(curr,begin,precision,'AltLin',deltaType)
-            c = linearLagrangeInterpolateLog(next,curr) 
-        elif estimationType == 'Linear':
-            next = search(curr,begin,precision,'Linear',deltaType)
-            c = linearLagrangeInterpolateF(next,curr)   
-        lines.append(c)
-        waypoint.append(next)
+        # print(curr)
+        if estimation_type == 'AltLin':
+            next = search(curr, begin, delta, 'AltLin', delta_type)
+            c = lineParameterLog(next, curr)
+            lines.append(c)
+            waypoint.append(next)
+        elif estimation_type == 'Linear':
+            next = search(curr, begin, delta, 'Linear', delta_type)
+            c = lineParameterF(next, curr)
+            lines.append(c)
+            waypoint.append(next)
         curr = next
-    return np.array(lines),np.array(waypoint)
+    return numpy.array(lines), numpy.array(waypoint)
 
-def Sn(data):
+
+def Sn(data: numpy.ndarray) -> numpy.ndarray:
+    """
+    return the sample covariance of data
+    :param data: numpy array of shape (n, d); n samples, d dimension
+    :return: the sample covariance of size (d, d)
+    """
     d = data[0].shape[0]
     n = len(data)
-    Sn = np.zeros((d,d))
+    s_n = numpy.zeros((d, d))
     for i in range(n):
-        X = np.expand_dims(data[i], axis=0).T
-        Sn += 1/n * X @ X.T
-    return Sn
+        x = numpy.expand_dims(data[i], axis=0).T
+        s_n += 1 / n * x @ x.T
+    return s_n
 
-    
-def is_pos_def(x):
-    return np.all(np.linalg.eigvals(x) > 0)
+
+def is_pos_def(x: numpy.ndarray) -> bool:
+    """
+    check if matrix x is positive-semi-definite
+    :param x: the numpy array of shape (k, k)
+    :return: return true if matrix x is positive-semi-definite
+    """
+    return numpy.all(numpy.linalg.eigvals(x) > 0)
 
 
 class LGC:
-    def __init__(self,cov,dimension, data = [],delta = 1e-3,epsilon = 5e-3,mu = 1e-2, errorType = 'max',debug = False):
+    def __init__(self, cov: cvxpy.Variable, dimension: int, data: numpy.ndarray,
+                 delta: float = 1e-3, epsilon: float = 5e-3, mu: float = 1e-2,
+                 error_type: str = 'max',
+                 debug: bool = False) -> None:
+        """
+        initialization of the parser
+        :param cov: the cvxpy variable for the covariance
+        :param dimension: the dimension d
+        :param data: the data of shape (n,d)
+        :param delta: maximum error
+        :param epsilon: min x coordinate
+        :param mu: estimation method split point
+        :param error_type: either 'snr' or 'max'
+        :param debug: True for debug mode
+        """
         self.dlt = delta
         self.eps = epsilon
         self.dim = dimension
-        self.errorType = errorType
+        self.errorType = error_type
         self.mu = mu
         self.constraints = []
         self.data = data
-        self.I = np.eye(self.dim)
+        self.identity = numpy.eye(self.dim)
         self.size = 0
         self.cov = cov
-        self.X = cp.Variable((self.dim,self.dim), PSD = True)
-        self.Y = cp.Variable((self.dim,self.dim))
-        self.t = cp.Variable()
-        self.lamd_1 = cp.Variable((self.dim,self.dim))
-        self.lamd_2 = cp.Variable((self.dim,self.dim))
-        self.X_2 = cp.Variable((self.dim,self.dim))
-        self.Y_2 = cp.Variable((self.dim,self.dim))
-        self.X_1 = cp.Variable((self.dim,self.dim))
-        self.Y_1 = cp.Variable((self.dim,self.dim))
+        self.X = cvxpy.Variable((self.dim, self.dim), PSD=True)
+        self.Y = cvxpy.Variable((self.dim, self.dim))
+        self.t = cvxpy.Variable()
+        self.lambda_1 = cvxpy.Variable((self.dim, self.dim))
+        self.lambda_2 = cvxpy.Variable((self.dim, self.dim))
+        self.X_2 = cvxpy.Variable((self.dim, self.dim))
+        self.Y_2 = cvxpy.Variable((self.dim, self.dim))
+        self.X_1 = cvxpy.Variable((self.dim, self.dim))
+        self.Y_1 = cvxpy.Variable((self.dim, self.dim))
         self.X_ = []
-        self.lamd = []
+        self.lambda_total = []
         self.tau = []
-        self.taup = []
+        self.tau_p = []
         self.debug = debug
+        self.prob = None
 
     def setup_constraint(self):
-        self.sigma = Sn(self.data)
-        self.sqrt_sigma = sp.linalg.sqrtm(self.sigma)
+        sigma = Sn(self.data)
+        sqrt_sigma = scipy.linalg.sqrtm(sigma)
 
-        self.linesFrL,self.waypointFrL = approximate(self.dlt,self.eps,self.mu,'AltLin',self.errorType)
-        self.linesLin,self.waypointLin = approximate(self.dlt,self.mu,2,'Linear',self.errorType)
-        if (self.debug):
-            print('{} alternate segments'.format(len(self.linesFrL)))
-            print('{} linear segments'.format(len(self.linesLin)))
-        #setup array variables
-        for j in range(len(self.linesFrL)):
-            self.lamd.append(cp.Variable((self.dim,self.dim), PSD = True))
-            self.X_.append(cp.Variable((self.dim,self.dim), PSD = True))
-            self.taup.append(cp.Variable((self.dim,self.dim), PSD = True))
-            self.tau.append(cp.Variable((self.dim,self.dim), PSD = True))
+        lines_alt_lin, waypoint_alt_lin = approximate(self.dlt, self.eps, self.mu, 'AltLin', self.errorType)
+        lines_lin, waypoint_lin = approximate(self.dlt, self.mu, 2, 'Linear', self.errorType)
+        if self.debug:
+            print('{} alternate segments'.format(len(lines_alt_lin)))
+            print('{} linear segments'.format(len(lines_lin)))
+        # setup array variables
+        for j in range(len(lines_alt_lin)):
+            self.lambda_total.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
+            self.X_.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
+            self.tau_p.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
+            self.tau.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
 
-        #AltLin portion
-        for j in range(len(self.linesFrL)):
-            a = self.linesFrL[j][0]
-            b = self.linesFrL[j][1]
-            self.constraints += [cp.bmat ([   [self.X_[j],self.lamd[j]],[self.lamd[j],self.taup[j]]]) >> 0 * np.eye(2 * self.dim)]
-            self.constraints += [self.taup[j] + a*self.X_[j] + b*self.lamd[j] << self.tau[j]]
+        # AltLin portion
+        for j in range(len(lines_alt_lin)):
+            a = lines_alt_lin[j][0]
+            b = lines_alt_lin[j][1]
+            self.constraints += [
+                cvxpy.bmat([[self.X_[j], self.lambda_total[j]],
+                            [self.lambda_total[j], self.tau_p[j]]])
+                >> 0 * numpy.eye(2 * self.dim)]
+            self.constraints += [self.tau_p[j] + a * self.X_[j] + b * self.lambda_total[j] << self.tau[j]]
 
-        self.constraints += [cp.sum(self.X_) == self.X_1]
-        self.constraints += [cp.sum(self.tau) == self.Y_1]
-        self.constraints += [cp.sum(self.lamd) == self.lamd_1]
+        self.constraints += [cvxpy.sum(self.X_) == self.X_1]
+        self.constraints += [cvxpy.sum(self.tau) == self.Y_1]
+        self.constraints += [cvxpy.sum(self.lambda_total) == self.lambda_1]
 
-        #Linear Portion
-        a = self.linesLin[:,0]
-        c = self.linesLin[:,1]
-        self.constraints += [a[i] * self.X_2 - self.Y_2 << -c[i] * self.lamd_2 for i in range(len(self.linesLin))]
+        # Linear Portion
+        a = lines_lin[:, 0]
+        c = lines_lin[:, 1]
+        self.constraints += [a[i] * self.X_2 - self.Y_2 << -c[i] * self.lambda_2 for i in range(len(lines_lin))]
 
-        self.constraints += [self.lamd_1 + self.lamd_2 == self.I]
+        self.constraints += [self.lambda_1 + self.lambda_2 == self.identity]
         self.constraints += [self.X_1 + self.X_2 == self.X]
         self.constraints += [self.Y_1 + self.Y_2 == self.Y]
 
+        self.constraints += [self.X << 2 * self.identity]
+        self.constraints += [self.X >> self.eps * self.identity]
+        self.constraints += [cvxpy.trace(self.Y) <= self.t]
 
-        self.constraints += [self.X << 2*self.I]
-        self.constraints += [self.X >> self.eps*self.I]
-        self.constraints += [cp.trace(self.Y) <= self.t]
-
-        self.constraints += [self.sqrt_sigma @ self.X @ self.sqrt_sigma == self.cov]
+        self.constraints += [sqrt_sigma @ self.X @ sqrt_sigma == self.cov]
 
     def add_constraint(self, constraints):
         self.constraints += constraints
 
-    def solve(self, solver = cp.SCS,verbose = True):
+    def solve(self, solver=cvxpy.SCS, verbose=True):
         self.setup_constraint()
-        prob = cp.Problem(cp.Minimize(self.t),self.constraints)
-        prob.solve(solver = solver, verbose = verbose)
-        self.time = prob.solver_stats.solve_time
-        return self.t.value, self.cov.value, self.time
+        self.prob = cvxpy.Problem(cvxpy.Minimize(self.t), self.constraints)
+        self.prob.solve(solver=solver, verbose=verbose)
+        time = self.prob.solver_stats.solve_time
+        return self.cov.value, time
+
