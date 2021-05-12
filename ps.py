400ae49b853bfbb533dabbade7fe629135295cf9
Chi Bach Pham <p.chi.bach@gmail.com>
2021-05-13 00:28:48 +1000

    Master (#3)
    * 1/05/21 Total Revamp
    
    * Ducumentation added
    
    * Bugfix

--- ps.py
+++ ps.py
@@ -102,6 +102,7 @@ def search(begin: float, end: float, delta: float, estimation_type: str = 'Linea
     pivot = begin
     while abs(begin - end) > 1e-9:
         mid = (begin + end) / 2
+
         if estimation_type == 'AltLin':
             [error, x_err] = altLinEstimatorError(pivot, mid)
             if delta_type == 'snr':
@@ -110,7 +111,6 @@ def search(begin: float, end: float, delta: float, estimation_type: str = 'Linea
             error, x_err = linearEstimatorError(pivot, mid)
             if delta_type == 'snr':
                 error = error / f(x_err)
-
 
         if error <= delta:
             begin = mid
@@ -150,6 +150,7 @@ def approximate(delta: float, begin: float, end: float,
         curr = next
     return numpy.array(lines), numpy.array(waypoint)
 
+
 def Sn(data: numpy.ndarray) -> numpy.ndarray:
     """
     return the sample covariance of data
@@ -172,7 +173,6 @@ def is_pos_def(x: numpy.ndarray) -> bool:
     :return: return true if matrix x is positive-semi-definite
     """
     return numpy.all(numpy.linalg.eigvals(x) > 0)
-
 
 
 class LGC:
@@ -201,15 +201,15 @@ class LGC:
         self.identity = numpy.eye(self.dim)
         self.size = 0
         self.cov = cov
-        self.X = cvxpy.Variable((self.dim, self.dim), PSD=True)
-        self.Y = cvxpy.Variable((self.dim, self.dim))
-        self.t = cvxpy.Variable()
-        self.lambda_1 = cvxpy.Variable((self.dim, self.dim))
-        self.lambda_2 = cvxpy.Variable((self.dim, self.dim))
-        self.X_2 = cvxpy.Variable((self.dim, self.dim))
-        self.Y_2 = cvxpy.Variable((self.dim, self.dim))
-        self.X_1 = cvxpy.Variable((self.dim, self.dim))
-        self.Y_1 = cvxpy.Variable((self.dim, self.dim))
+        self.X = cvxpy.Variable((self.dim, self.dim), PSD=True,name = 'X')
+        self.Y = cvxpy.Variable((self.dim, self.dim),name = 'Y')
+        self.t = cvxpy.Variable(name = 't')
+        self.lambda_1 = cvxpy.Variable((self.dim, self.dim),name = 'lambda 1')
+        self.lambda_2 = cvxpy.Variable((self.dim, self.dim),name = 'lambda 2')
+        self.X_2 = cvxpy.Variable((self.dim, self.dim),name = 'X 2')
+        self.Y_2 = cvxpy.Variable((self.dim, self.dim),name = 'Y 2')
+        self.X_1 = cvxpy.Variable((self.dim, self.dim),name = 'X 1')
+        self.Y_1 = cvxpy.Variable((self.dim, self.dim),name = 'Y 1')
         self.X_ = []
         self.lambda_total = []
         self.tau = []
@@ -218,8 +218,9 @@ class LGC:
         self.prob = None
 
     def setup_constraint(self):
-        sigma = Sn(self.data)
-        sqrt_sigma = scipy.linalg.sqrtm(sigma)
+        if len(self.data) > 0:
+            sigma = Sn(self.data)
+            sqrt_sigma = scipy.linalg.sqrtm(sigma)
 
         lines_alt_lin, waypoint_alt_lin = approximate(self.dlt, self.eps, self.mu, 'AltLin', self.errorType)
         lines_lin, waypoint_lin = approximate(self.dlt, self.mu, 2, 'Linear', self.errorType)
@@ -227,41 +228,73 @@ class LGC:
             print('{} alternate segments'.format(len(lines_alt_lin)))
             print('{} linear segments'.format(len(lines_lin)))
         # setup array variables
-        for j in range(len(lines_alt_lin)):
-            self.lambda_total.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
-            self.X_.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
-            self.tau_p.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
-            self.tau.append(cvxpy.Variable((self.dim, self.dim), PSD=True))
+        if len(lines_alt_lin) > 0:
+            for j in range(len(lines_alt_lin)):
+                self.lambda_total.append(cvxpy.Variable((self.dim, self.dim),name = 'lambda_ ' + str(j)))
+                self.X_.append(cvxpy.Variable((self.dim, self.dim),name = 'X_ ' + str(j)))
+                self.tau_p.append(cvxpy.Variable((self.dim, self.dim),name = 'tau p ' + str(j)))
+                self.tau.append(cvxpy.Variable((self.dim, self.dim),name = 'tau ' + str(j)))
 
-        # AltLin portion
-        for j in range(len(lines_alt_lin)):
-            a = lines_alt_lin[j][0]
-            b = lines_alt_lin[j][1]
-            self.constraints += [
-                cvxpy.bmat([[self.X_[j], self.lambda_total[j]],
-                            [self.lambda_total[j], self.tau_p[j]]])
-                >> 0 * numpy.eye(2 * self.dim)]
-            self.constraints += [self.tau_p[j] + a * self.X_[j] + b * self.lambda_total[j] << self.tau[j]]
+            # AltLin portion
+            for j in range(len(lines_alt_lin)):
+                a = lines_alt_lin[j][0]
+                b = lines_alt_lin[j][1]
 
-        self.constraints += [cvxpy.sum(self.X_) == self.X_1]
-        self.constraints += [cvxpy.sum(self.tau) == self.Y_1]
-        self.constraints += [cvxpy.sum(self.lambda_total) == self.lambda_1]
+                self.constraints += [self.X_[j] << waypoint_alt_lin[j] * self.lambda_total[j]]
+                if j < len(lines_alt_lin)-1:
+                    self.constraints += [self.X_[j] >> waypoint_alt_lin[j+1] * self.lambda_total[j]]
+                else:
+                    self.constraints += [self.X_[j] >> self.eps * self.lambda_total[j]]
+
+                self.constraints += [self.lambda_total[j] << self.identity]
+
+                self.constraints += [
+                    cvxpy.bmat([[self.X_[j], self.lambda_total[j]],
+                                [self.lambda_total[j], self.tau_p[j]]])
+                    >> 0 * numpy.eye(2 * self.dim)]
+                self.constraints += [self.tau_p[j] + a * self.X_[j] + b * self.lambda_total[j] << self.tau[j]]
+
+            self.constraints += [self.lambda_1 << self.identity]
+            self.constraints += [cvxpy.sum(self.X_) == self.X_1]
+            self.constraints += [cvxpy.sum(self.tau) == self.Y_1]
+            self.constraints += [cvxpy.sum(self.lambda_total) == self.lambda_1]
 
         # Linear Portion
-        a = lines_lin[:, 0]
-        c = lines_lin[:, 1]
-        self.constraints += [a[i] * self.X_2 - self.Y_2 << -c[i] * self.lambda_2 for i in range(len(lines_lin))]
+        if len(lines_lin) > 0:
+            a = lines_lin[:, 0]
+            c = lines_lin[:, 1]
+            self.constraints += [a[i] * self.X_2 - self.Y_2 << -c[i] * self.lambda_2 for i in range(len(lines_lin))]
 
-        self.constraints += [self.lambda_1 + self.lambda_2 == self.identity]
-        self.constraints += [self.X_1 + self.X_2 == self.X]
-        self.constraints += [self.Y_1 + self.Y_2 == self.Y]
+        
+        self.constraints += [self.X_1 << self.lambda_1 * self.mu]
+        self.constraints += [self.X_1 >> self.lambda_1 * self.eps]
+
+        self.constraints += [self.X_2 << self.lambda_2 * 2]
+        self.constraints += [self.X_2 >> self.lambda_2 * self.mu]
+
+        if len(lines_lin) > 0 and len(lines_alt_lin) > 0:
+            self.constraints += [self.X_1 + self.X_2 == self.X]
+            self.constraints += [self.Y_1 + self.Y_2 == self.Y]
+            self.constraints += [self.lambda_1 + self.lambda_2 == self.identity]
+        elif len(lines_lin) > 0 :
+            self.constraints += [self.X_2 == self.X]
+            self.constraints += [self.Y_2 == self.Y]
+            self.constraints += [self.lambda_2 == self.identity]
+        else:
+            self.constraints += [self.X_1 == self.X]
+            self.constraints += [self.Y_1 == self.Y]
+            self.constraints += [self.lambda_1 == self.identity]
+
+
 
         self.constraints += [self.X << 2 * self.identity]
         self.constraints += [self.X >> self.eps * self.identity]
+
         self.constraints += [cvxpy.trace(self.Y) <= self.t]
-
-        self.constraints += [sqrt_sigma @ self.X @ sqrt_sigma == self.cov]
-
+        if len(self.data) > 0:
+            self.constraints += [sqrt_sigma @ self.X @ sqrt_sigma == self.cov]
+        else:
+            self.constraints += [self.X == self.cov]
     def add_constraint(self, constraints):
         self.constraints += constraints
 
@@ -269,6 +302,7 @@ class LGC:
         self.setup_constraint()
         self.prob = cvxpy.Problem(cvxpy.Minimize(self.t), self.constraints)
         self.prob.solve(solver=solver, verbose=verbose)
+
         time = self.prob.solver_stats.solve_time
         return self.cov.value, time
 
